91612fddd74d1dc22a487cd987815bbf613821e0
Andreea0999 <andreeacristutiu99@gmail.com>
2021-01-09 14:07:27 +0200

    Add project files.

---
+++ Cristutiu_Andreea_Lab12/Cristutiu_Andreea_Lab12/Data/IRestService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cristutiu_Andreea_Lab12.Models;
+namespace Cristutiu_Andreea_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
